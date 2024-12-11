f693ed27e1e7f9787b743b4667294a78af81d722
Raju k b <kbraju788@gmail.com>
2024-12-11 15:51:08 +0530

    Create sum of number in a given range.py

---
+++ sum of number in a given range.py
@@ -0,0 +1,7 @@
+#sum of number in a given range
+start = int(input ("enter a starting number."))
+end   = int(input("enter a ending number."))
+sum = 0
+for i in range(start,end+1):
+  sum +=1
+print("the sum is :",sum)
