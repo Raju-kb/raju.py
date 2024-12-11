4e13770fe912ae317e0c2f794ecf5053b51cfe5a
Raju k b <kbraju788@gmail.com>
2024-12-11 16:08:00 +0530

    Create if a number is greater than 100 but less than 500.py

---
+++ if a number is greater than 100 but less than 500.py
@@ -0,0 +1,5 @@
+number =int(input("enter a number :"))
+if number >100 and number <500:
+  print(f"{number} is greater than 100 and less than 500")+
+else:
+    print(f"{number} is not in the range 101 to 499.")
