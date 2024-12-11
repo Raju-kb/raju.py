2e150eef69bde565969162d95b112e12d2c5d6c3
Raju k b <kbraju788@gmail.com>
2024-12-11 15:55:10 +0530

    Create even or odd or zero.py

---
+++ even or odd or zero.py
@@ -0,0 +1,8 @@
+#check number is even or odd or zero
+number = int(input("Enter a number"))
+if number > 0:
+  print("number is positive")
+elif number < 0:
+  print("number is negetive")
+else:
+  print("zero")
