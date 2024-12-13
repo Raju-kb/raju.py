2c9e347132097cc51056942b9081ff5f99491a72
Raju k b <kbraju788@gmail.com>
2024-12-13 16:19:47 +0530

    Create magic 8 ball.py

---
+++ magic 8 ball.py
@@ -0,0 +1,15 @@
+import random
+responses = [
+    "it is certain",
+    "it is decidedly so",
+    "without a doubt",
+    "yes definitely",
+    "you may rely on it",
+    "as I see it,yes",
+    "most likely",
+    "outlook good",
+    "yes"
+]
+print("ask the magic 8 ball question:")
+input()
+print(random.choice(responses))
